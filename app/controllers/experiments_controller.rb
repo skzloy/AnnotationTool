635f0d0a75281909b91190dd742d631d13c4d1a2
Dmitry Chetverikov <skzloy@yandex.ru>
2014-06-10 20:17:35 +0400

    experiments further dev

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -2,9 +2,14 @@ class ExperimentsController < ApplicationController
  def create
     @article = Article.find(params[:article_id])
     @experiment = @article.experiments.create(experiment_params)
-	redirect_to article_path(@article)
+	  redirect_to article_experiment_path(@experiment)
   end
- 
+
+ def show
+   @experiment = Experiment.find(params[:id])
+   @blocks = @experiment.article.text.chars.each_slice(@experiment.blockSize).map(&:join)
+ end
+
   private
     def experiment_params
       params.require(:experiment).permit(:blockSize)
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   resources :articles do
 	resources :experiments
-  end	
+  end
+
+  resources :experiments
 
   #get 'welcome/index'
 
