0978a6073de7e93bd5eabacc3f4c835dd46d6078
skzloy <skzloy@yandex.ru>
2014-06-16 15:58:08 +0400

    block division bug fixed

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -1,26 +1,60 @@
-include ActionView::Helpers::TextProcessor
+# encoding: utf-8
 
 class ExperimentsController < ApplicationController
+
  def create
     @article = Article.find(params[:article_id])
     @experiment = @article.experiments.create(experiment_params)
-	  redirect_to article_experiment_path(@experiment)
+	  redirect_to article_experiment_path(@article, @experiment)
   end
 
  def show
    @article = Article.find(params[:article_id])
    @experiment = @article.experiments.find(params[:id])
-   @blocks = @experiment.article.text.chars.each_slice(@experiment.blockSize).map(&:join)
+   words = @experiment.article.text.split(' ')
+   @blocks = Array.new
+   blockSize = 0
+   block = ''
+   for word in words
+     blockSize += word.size
+
+     if blockSize <= @experiment.blockSize
+       block += word + ' '
+     else
+       @blocks.push(block)
+       blockSize = word.size
+       block = word + ' '
+
+     end
+
+
+
+
+   end
+   #@blocks = @experiment.article.text.chars.each_slice(@experiment.blockSize).map(&:join)
+   @word_count = get_word_count_by_blocks(@blocks)
  end
 
   private
+   class TextProcessor
+     def self.word_count(text)
+       return text.gsub(/[^\p{Cyrillic}]/, ' ').split.size
+     end
+
+     def self.sign_count(text)
+       return text.gsub(' ', '').size
+     end
+   end
+
     def experiment_params
       params.require(:experiment).permit(:blockSize)
     end
 
     def get_word_count_by_blocks(blocks)
-     for block in blocks
-       TextProcessor.word_count(block);
-     end
+      result = Array.new
+      blocks.each { |block|
+        result.push(TextProcessor.word_count(block))
+      }
+      return result
     end
 end
--- app/helpers/experiments_helper.rb
+++ app/helpers/experiments_helper.rb
@@ -1,10 +1,10 @@
 module ExperimentsHelper
   class TextProcessor
-    def self.word_count(text)
+    def word_count(text)
       return text.gsub(/[^-a-zA-Z]/, ' ').split.size
     end
 
-    def self.sign_count(text)
+    def sign_count(text)
       return text.gsub(' ', '').size
     end
   end
