8ccf8243ad9a4958438dc9816e51f1adecf09468
skzloy <skzloy@yandex.ru>
2014-06-18 11:06:44 +0400

    string variable for text switched to text

--- db/migrate/20140528143355_create_articles.rb
+++ db/migrate/20140528143355_create_articles.rb
@@ -2,7 +2,7 @@ class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
       t.string :title
-      t.string :text
+      t.text :text
 
       t.timestamps
     end
