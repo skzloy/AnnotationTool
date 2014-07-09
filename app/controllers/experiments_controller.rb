e8cb95ea624030adbca63603908855650f320575
skzloy <skzloy@yandex.ru>
2014-07-10 00:23:00 +0400

    substring matches added

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -15,7 +15,7 @@ class ExperimentsController < ApplicationController
    @blocks = Array.new
    blockSize = 0
    textBlock = ''
-   blockCount = 0
+   blockCount = 1
    for word in words
      blockSize += word.size
 
@@ -35,6 +35,7 @@ class ExperimentsController < ApplicationController
    sign_count = get_sign_count_by_blocks(@blocks)
    word_count_divided_by_sign_count = word_count.zip(sign_count).map {|a| a.inject(1.0, :/)}
    @blocks_by_word_count_divided_by_sign_count = Hash[(0..@blocks.size).to_a.zip word_count_divided_by_sign_count]
+   @stringMatches = MatchesFinder.FindMatchesSubstring(@blocks.map{|block| block.EspChar.ESP3.GroupID}.join(''))
  end
 
   private
@@ -74,12 +75,12 @@ class ExperimentsController < ApplicationController
     class ESP6
       attr_reader :E1, :E2, :E3, :E4, :E5, :E6
       def initialize(block)
-        @E1 = sign_count(block, "птк")
-        @E2 = sign_count(block, "сшфцхщ")
-        @E3 = sign_count(block, "бдг")
-        @E4 = sign_count(block, "взж")
-        @E5 = sign_count(block, "мн")
-        @E6 = sign_count(block, "лрй")
+        @E1 = sign_count(block, 'птк')
+        @E2 = sign_count(block, 'сшфцхщ')
+        @E3 = sign_count(block, 'бдг')
+        @E4 = sign_count(block, 'взж')
+        @E5 = sign_count(block, 'мн')
+        @E6 = sign_count(block, 'лрй')
       end
 
       private
@@ -104,9 +105,9 @@ class ExperimentsController < ApplicationController
     end
 
     class ESP3
-      attr_reader :E1, :E2, :E3, :EP1, :EP2, :EP3
+      attr_reader :E1, :E2, :E3, :EP1, :EP2, :EP3, :GroupID
       def initialize(esp6)
-        if(esp6.instance_of?(ESP6))
+        if esp6.instance_of?(ESP6)
           @E1 = esp6.E1 + esp6.E2
           @E2 = esp6.E3 + esp6.E4
           @E3 = esp6.E5 + esp6.E6
@@ -170,8 +171,86 @@ class ExperimentsController < ApplicationController
               end
             end
           end
+
+          @GroupID = ESP3GroupProvider.GetGroup(@EP1.to_s + @EP2.to_s + @EP3.to_s)
         end
 
+      end
+    end
+
+    class ESP3GroupProvider
+      def self.GetGroup(esp3p)
+        if @groups == nil
+          @groups = Hash.new
+          @groups['112'] = 'a'
+          @groups['121'] = 'b'
+          @groups['122'] = 'c'
+          @groups['123'] = 'd'
+          @groups['132'] = 'e'
+
+          @groups['222'] = 'f'
+          @groups['212'] = 'g'
+          @groups['213'] = 'h'
+          @groups['221'] = 'i'
+          @groups['223'] = 'q'
+          @groups['231'] = 'j'
+          @groups['232'] = 'k'
+
+          @groups['312'] = 'l'
+          @groups['321'] = 'm'
+          @groups['322'] = 'n'
+          @groups['312'] = 'o'
+          @groups['332'] = 'p'
+        end
+
+        return @groups[esp3p]
+      end
+    end
+
+    class MatchesFinder
+      def self.FindMatchesSubstring(st)
+        originalString = st
+        matches = Hash.new
+        res = Array.new
+
+        for j in 2..5
+          for i in 0..originalString.size
+            if originalString.size - i < j
+              break
+            end
+
+            substringToSearch = originalString[i, j]
+            if (originalString.include? substringToSearch) && (matches[substringToSearch] == nil)
+
+              matchCount = 0
+              startBlockID = Array.new
+              originalString.scan(substringToSearch) do |c|
+                startBlockID << $~.offset(0)[0]
+                matchCount += 1
+              end
+
+              if(matchCount > 1)
+                ms = MatchingSubstring.new(substringToSearch, substringToSearch.size, matchCount, startBlockID)
+                matches[substringToSearch] = ms
+                res.push(ms)
+              end
+
+
+            end
+          end
+        end
+
+        return res
+      end
+    end
+
+    class MatchingSubstring
+      attr_reader :Substring, :Lenght, :MatchingCount, :StartBlocksID
+      def initialize(substring, lenght, matchingCount, startBlockID)
+        @Substring = substring
+        @Lenght = lenght
+        @MatchingCount = matchingCount
+        @StartBlocksID = startBlockID
       end
     end
 
